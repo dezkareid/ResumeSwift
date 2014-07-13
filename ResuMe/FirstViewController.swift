7e2486397705ad2b97d9167e1e98a00d16bc1131
dezkareid <elmaildeldezkareid@gmail.com>
2014-07-13 00:34:39 -0500

    Initial Commit

---
+++ ResuMe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ResuMe
+//
+//  Created by Joel Humberto Gómez Paredes on 13/07/14.
+//  Copyright (c) 2014 Joel Humberto Gómez Paredes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
