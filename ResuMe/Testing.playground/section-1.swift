f52ac8e6b8dd670ca39e04fd25bee86f3a76ec9c
dezkareid <elmaildeldezkareid@gmail.com>
2014-07-13 05:18:58 -0500

    Código para descargar en json agregado, solo falta agregarle parámetros y personalizar funciones para cada parte del curriculum

--- ResuMe/Testing.playground/section-1.swift
+++ ResuMe/Testing.playground/section-1.swift
@@ -3,3 +3,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+var urlPath = "https://script.google.com/macros/s/AKfycbyx9FM2Athv6v0yuKzES1jZI7sCdqbY3mbtcnzrdnSblsmh0AQ/exec"
+var url: NSURL = NSURL(string: urlPath)
+var session = NSURLSession.sharedSession()
+var task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+    println("Task completed")
+    if(error) {
+        // If there is an error in the web request, print it to the console
+        println(error.localizedDescription)
+    }
+    var err: NSError?
+    var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+    if(err?) {
+        // If there is an error parsing JSON, print it to the console
+        println("JSON Error \(err!.localizedDescription)")
+    }
+    var results: NSArray = jsonResult["cursos"] as NSArray
+    dispatch_async(dispatch_get_main_queue(), {
+        println(results.count)
+        })
+    })
+task.resume()
